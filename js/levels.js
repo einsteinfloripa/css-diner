fc391e69c1a94a9e526da70a138073caac2e1680
Djonys Dalmy <djonysdalmy@gmail.com>
2021-11-27 23:48:56 -0300

    Fix bugs and more translation

--- js/levels.js
+++ js/levels.js
@@ -55,7 +55,7 @@ var levels = [
     doThis : "Selecione a Maçã que está no prato",
     selector : "prato maçã",
     syntax: "A&nbsp;&nbsp;B",
-    help : "Selects all <strong>B</strong> inside of <strong>A</strong>. <strong>B</strong> is called a descendant because it is inside of another element.",
+    help : "",
     examples : [
       '<strong>p&nbsp;&nbsp;strong</strong> selects all <tag>strong</tag> elements that are inside of any <tag>p</tag>',
       '<strong>#fancy&nbsp;&nbsp;span</strong> selects any <tag>span</tag> elements that are inside of the element with <strong>id="fancy"</strong>',
@@ -73,7 +73,7 @@ var levels = [
     selector : "#chique picles",
     helpTitle: "Combine the Descendant & ID Selectors",
     syntax: "#id&nbsp;&nbsp;A",
-    help : 'You can combine any selector with the descendent selector.',
+    help : '',
     examples : [
       '<strong>#cool&nbsp;span</strong> selects all <tag>span</tag> elements that are inside of elements with <strong>id="cool"</strong>'
     ],
@@ -95,7 +95,7 @@ var levels = [
     selectorName: "Seletor por classe",
     helpTitle: "Select elements by their class",
     syntax: ".classname",
-    help : 'The class selector selects all elements with that class attribute. Elements can only have one ID, but many classes.',
+    help : '',
     examples : [
     '<strong>.neato</strong> selects all elements with <strong>class="neato"</strong>'
     ],
@@ -113,7 +113,7 @@ var levels = [
     selector : "laranja.pequeno",
     helpTitle: "Combine the Class Selector",
     syntax: "A.className",
-    help : 'You can combine the class selector with other selectors, like the type selector.',
+    help : '',
     examples : [
       '<strong>ul.important</strong> selects all <tag>ul</tag> elements that have <strong>class="important"</strong>',
       '<strong>#big.wide</strong> selects all elements with <strong>id="big"</strong> that also have <strong>class="wide"</strong>'
@@ -136,7 +136,7 @@ var levels = [
     selector : "toalha laranja.pequeno",
     syntax: "Put your back into it!",
     helpTitle: "You can do it...",
-    help : 'Combine what you learned in the last few levels to solve this one!',
+    help : '',
     boardMarkup : `
     <toalha>
       <laranja/>
@@ -159,7 +159,7 @@ var levels = [
     selectorName : "Combinador por vírgula",
     helpTitle: "Combine, selectors, with... commas!",
     syntax : "A, B",
-    help : 'Thanks to Shatner technology, this selects all <strong>A</strong> and <strong>B</strong> elements. You can combine any selectors this way, and you can specify more than two.',
+    help : '',
     examples: [
     '<strong>p, .fun</strong> selects all <tag>p</tag> elements as well as all elements with <strong>class="fun"</strong>',
     '<strong>a, p, div</strong> selects all <tag>a</tag>, <tag>p</tag> and <tag>div</tag> elements'
@@ -186,7 +186,7 @@ var levels = [
     selectorName:  "Seletor universal",
     helpTitle: "You can select everything!",
     syntax : "*",
-    help : 'You can select all elements with the universal selector! ',
+    help : '',
     examples : [
       '<strong>p *</strong> selects any element inside all <tag>p</tag> elements.',
     ],
@@ -207,7 +207,7 @@ var levels = [
     selector : "prato *",
     syntax : "A&nbsp;&nbsp;*",
     helpTitle: "Combine the Universal Selector",
-    help : 'This selects all elements inside of <strong>A</strong>.',
+    help : '',
     examples : [
       '<strong>p *</strong> selects every element inside all <tag>p</tag> elements.',
       '<strong>ul.fancy *</strong> selects every element inside all <tag>ul class="fancy"</tag> elements.'
@@ -230,7 +230,7 @@ var levels = [
     helpTitle: "Select an element that directly follows another element",
     selectorName: "Irmãos Adjacentes",
     syntax : "A + B",
-    help : "This selects all <strong>B</strong> elements that directly follow <strong>A</strong>. Elements that follow one another are called siblings. They're on the same level, or depth. <br/><br/>In the HTML markup for this level, elements that have the same indentation are siblings.",
+    help : "",
     examples : [
       '<strong>p + .intro</strong> selects every element with <strong>class="intro"</strong> that directly follows a <tag>p</tag>',
       '<strong>div + a</strong> selects every <tag>a</tag> element that directly follows a <tag>div</tag>'
@@ -253,7 +253,7 @@ var levels = [
     syntax: "A ~ B",
     doThis : "Selecione os picles que estão depois da toalha",
     selector : "toalha ~ picles",
-    help : "You can select all siblings of an element that follow it. This is like the Adjacent Selector (A + B) except it gets all of the following elements instead of one.",
+    help : "",
     examples : [
       '<strong>A ~ B</strong> selects all <strong>B</strong> that follow a <strong>A</strong>'
     ],
@@ -278,7 +278,7 @@ var levels = [
     doThis : "Selecione a Maçã que está diretamente em cima de um prato",
     selector : "prato > maçã",
     helpTitle: "Select direct children of an element",
-    help : "You can select elements that are direct children of other elements. A child element is any element that is nested directly in another element. <br><br>Elements that are nested deeper than that are called descendant elements.",
+    help : "",
     examples : [
       '<strong>A > B</strong> selects all <strong>B</strong> that are a direct children <strong>A</strong>'
     ],
@@ -302,8 +302,7 @@ var levels = [
     doThis : "Selecione a laranja de cima",
     selector : "prato :first-child",
     syntax: ":first-child",
-
-    help : "You can select the first child element. A child element is any element that is directly nested in another element. You can combine this pseudo-selector with other selectors.",
+    help : "",
     examples : [
       '<strong>:first-child</strong> selects all first child elements.',
       '<strong>p:first-child</strong> selects all first child <tag>p</tag> elements.',
@@ -326,7 +325,7 @@ var levels = [
     doThis : "Selecione a Maçã e o Picles que estão em um prato",
     selector : "prato :only-child",
     syntax: ":only-child",
-    help : "You can select any element that is the only element inside of another one.",
+    help : "",
     examples : [
       '<strong>span:only-child</strong> selects the <tag>span</tag> elements that are the only child of some other element.',
       '<strong>ul li:only-child</strong> selects the only <tag>li</tag> element that are in a <tag>ul</tag>.'
@@ -354,7 +353,7 @@ var levels = [
     doThis : "Selecione a Maçã e o Picles pequenos",
     selector : ".pequeno:last-child",
     syntax: ":last-child",
-    help : "You can use this selector to select an element that is the last child element inside of another element. <br><br>Pro Tip &rarr; In cases where there is only one element, that element counts as the first-child, only-child and last-child!",
+    help : "",
     examples : [
       '<strong>:last-child</strong> selects all last-child elements.',
       '<strong>span:last-child</strong> selects all last-child <tag>span</tag> elements.',
@@ -377,7 +376,7 @@ var levels = [
     doThis : "Selecione o terceiro prato",
     selector : ":nth-child(3)",
     syntax: ":nth-child(A)",
-    help : "Selects the <strong>nth</strong> (Ex: 1st, 3rd, 12th etc.) child element in another element.",
+    help : "",
     examples : [
       '<strong>:nth-child(8)</strong> selects every element that is the 8th child of another element.',
       '<strong>div p:nth-child(2)</strong> selects the second <strong>p</strong> in every <strong>div</strong>',
@@ -395,7 +394,7 @@ var levels = [
     doThis : "selecione a primeira toalha",
     selector : "toalha:nth-last-child(3)",
     syntax: ":nth-last-child(A)",
-    help : "Selects the children from the bottom of the parent. This is like nth-child, but counting from the back!",
+    help : "",
     examples : [
       '<strong>:nth-last-child(2)</strong> selects all second-to-last child elements.'
     ],
@@ -416,7 +415,7 @@ var levels = [
     doThis : "Selecione a primeira Maçã",
     selector : "maçã:first-of-type",
     syntax: ":first-of-type",
-    help : "Selects the first element of that type within another element.",
+    help : "",
     examples : [
       '<strong>span:first-of-type</strong> selects the first <tag>span</tag> in any element.'
     ],
@@ -437,7 +436,7 @@ var levels = [
     doThis: "Selecione todos os pratos pares",
     selector: "prato:nth-of-type(even)",
     syntax: ":nth-of-type(A)",
-    help: "Selects a specific element based on its type and order in another element - or even or odd instances of that element.",
+    help: "",
     examples: [
       '<strong>div:nth-of-type(2)</strong> selects the second instance of a div.',
       '<strong>.example:nth-of-type(odd)</strong> selects all odd instances of a the example class.'
@@ -456,7 +455,7 @@ var levels = [
     doThis: "Selecione cada segundo prato, começando do terceiro",
     selector: "prato:nth-of-type(2n+3)",
     syntax: ":nth-of-type(An+B)",
-    help: "The nth-of-type formula selects every nth element, starting the count at a specific instance of that element.",
+    help: "",
     examples: [
       '<strong>span:nth-of-type(6n+2)</strong> selects every 6th instance of a <tag>span</tag>, starting from (and including) the second instance.'
     ],
@@ -481,7 +480,7 @@ var levels = [
     selector : "maçã:only-of-type",
     syntax: ":only-of-type",
     doThis : "Selecione a Maçã no prato do meio",
-    help : "Selects the only element of its type within another element.",
+    help : "",
     examples : [
       '<strong>p span:only-of-type</strong> selects a <tag>span</tag> within any <tag>p</tag> if it is the only <tag>span</tag> in there.'
     ],
@@ -504,7 +503,7 @@ var levels = [
     doThis : "Selecione a última maçã e laranja",
     selector : ".pequeno:last-of-type",
     syntax: ":last-of-type",
-    help : "Selects each last element of that type within another element. Remember type refers the kind of tag, so <tag>p</tag> and <tag>span</tag> are different types. <br><br> I wonder if this is how the last dinosaur was selected before it went extinct.",
+    help : "",
     examples : [
       '<strong>div:last-of-type</strong> selects the last <tag>div</tag> in every element.',
       '<strong>p span:last-of-type</strong> selects the last <tag>span</tag> in every <tag>p</tag>.'
@@ -524,7 +523,7 @@ var levels = [
     doThis : "Selecione as toalhas vazias",
     selector : "toalha:empty",
     syntax: ":empty",
-    help : "Selects elements that don't have any other elements inside of them.",
+    help : "",
     examples : [
       '<strong>div:empty</strong> selects all empty <tag>div</tag> elements.'
     ],
@@ -542,7 +541,7 @@ var levels = [
     doThis : "Selecione as Maçãs grandes",
     selector : "maçã:not(.pequeno)",
     syntax: ":not(X)",
-    help : 'You can use this to select all elements that do not match selector <strong>"X"</strong>.',
+    help : '',
     examples : [
       '<strong>:not(#fancy)</strong> selects all elements that do not have <strong>id="fancy"</strong>.',
       '<strong>div:not(:first-child)</strong> selects every <tag>div</tag> that is not a first child.',
@@ -568,7 +567,7 @@ var levels = [
     doThis : "Selecione os itens que são para alguém",
     selector : "[for]",
     syntax: "[attribute]",
-    help : 'Attributes appear inside the opening tag of an element, like this: <tag>span attribute="value"</tag>. An attribute does not always have a value, it can be blank!',
+    help : '',
     examples : [
       '<strong>a[href]</strong> selects all <tag>a</tag> elements that have a <strong>href="anything"</strong> attribute.',
       '<strong>[type]</strong> selects all elements that have a <strong>type="anything"</strong>. attribute'
@@ -586,7 +585,7 @@ var levels = [
     doThis : "Selecione os pratos que são para alguém",
     selector : "prato[for]",
     syntax: "A[attribute]",
-    help : "Combine the attribute selector with another selector (like the tag name selector) by adding it to the end.",
+    help : "",
     examples : [
       '<strong>[value]</strong> selects all elements that have a <strong>value="anything"</strong> attribute.',
       '<strong>a[href]</strong> selects all <tag>a</tag> elements that have a <strong>href="anything"</strong> attribute.',
@@ -605,7 +604,7 @@ var levels = [
     doThis : "Selecione a refeição de Vitaly",
     selector : "[for=Vitaly]",
     syntax: '[attribute="value"]',
-    help : "Attribute selectors are case sensitive, each character must match exactly.",
+    help : "",
     examples : [
       '<strong>input[type="checkbox"]</strong> selects all checkbox input elements.'
     ],
@@ -656,7 +655,7 @@ var levels = [
     syntax: '[attribute*="value"]',
     doThis : "Selecione as refeições para nomes que contenham 'obb'",
     selector : '[for*="obb"]',
-    help : 'A useful selector if you can identify a common pattern in things like <strong>class</strong>, <strong>href</strong> or <strong>src</strong> attributes.',
+    help : '',
     examples : [
       '<strong>img[src*="/thumbnails/"]</strong> selects all image elements that show images from the "thumbnails" folder.',
       '<strong>[class*="heading"]</strong> selects all elements with "heading" in their class, like <strong>class="main-heading"</strong> and <strong>class="sub-heading"</strong>'
--- js/restaurant.js
+++ js/restaurant.js
@@ -630,7 +630,7 @@ function loadLevel(){
   loadBoard();
   resetTable();
 
-  $(".level-header .level-text").html("Level " + (currentLevel+1) + " of " + levels.length);
+  $(".level-header .level-text").html("Nível " + (currentLevel+1) + " de " + levels.length);
 
   updateProgressUI(currentLevel, checkCompleted(currentLevel));
 
